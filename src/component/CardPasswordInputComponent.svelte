dd13eac9c95d226df25926429a3d4e4c0209851f
SoftwareSing <softwaresing@gmail.com>
2021-09-30 21:56:37 +0800

    更新SC申請頁關於交易密碼的說明

--- src/component/CardPasswordInputComponent.svelte
+++ src/component/CardPasswordInputComponent.svelte
@@ -3,7 +3,7 @@
   export let password = ''
 </script>
 
-<input class="{inputClass}" type="password" bind:value={password} />
+<input class="{inputClass}" type="password" placeholder="發卡信上印製的交易密碼，請以硬幣刮開密碼條後確認" bind:value={password} />
 
 <style>
 </style>
